f1e2b5d08d7904671db28708ed914e6e3a3488fe
daviddvali <dato11235813@gmail.com>
2024-06-09 20:56:01 +0400

    added player idle, walk, run animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,32 +6,67 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
 
     public float walkSpeed = 5f; 
+    public float runSpeed = 8f;
 
     Vector2 moveInput;
 
+    private bool _isMoving;
+
+    public bool IsMoving {
+        get {
+            return _isMoving;
+        }
+        private set {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+        private bool _isRunning;
+
+    public bool IsRunning {
+        get {
+            return _isRunning;
+        }
+        private set {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
+
     void Awake() {
-        rb = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        rigidBody.velocity = new Vector2(moveInput.x * (IsRunning ? runSpeed : walkSpeed), rigidBody.velocity.y);
     }
 
-    void FixedUpdate() {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
-    }
+    // void FixedUpdate() {
+    //     rigidBody.velocity = new Vector2(moveInput.x * walkSpeed, rigidBody.velocity.y);
+    // }
 
-    public void onMove(InputAction.CallbackContext context) {
+    public void OnMove(InputAction.CallbackContext context) {
         moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+    }
+
+    public void OnRun(InputAction.CallbackContext context) {
+        if (context.started) {
+            IsRunning = true;
+        } else if (context.canceled) {
+            IsRunning = false;            
+        }
     }
 }
