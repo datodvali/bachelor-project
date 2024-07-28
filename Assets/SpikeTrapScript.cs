e71b3ef526ce04f88c88f9ff6237b36dc8082660
daviddvali <dato11235813@gmail.com>
2024-07-28 20:01:59 +0400

    added spike traps

---
+++ Assets/SpikeTrapScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SpikeTrapScript : MonoBehaviour
+{
+    [SerializeField] private int _damage = 100;
+    private Vector2 _knockBack = Vector2.up * 5;
+    private AudioSource _sfx;
+
+    void Awake() {
+        _sfx = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.TryGetComponent<Damageable>(out var damageable)) {
+            damageable.OnHit(_damage, _knockBack);
+            if (_sfx != null) {
+                AudioSource.PlayClipAtPoint(_sfx.clip, gameObject.transform.position, _sfx.volume);
+            }
+        }
+    }
+}
