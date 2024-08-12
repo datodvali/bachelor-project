523b875f8cd1a10dde74464a41357e6f8465bc20
daviddvali <dato11235813@gmail.com>
2024-08-12 22:40:52 +0400

    added vertical moving platform + moving spike trap

---
+++ Assets/Scripts/PlatformScripts/VerticalCollisionBasedMovingPlatform.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class VerticalCollisionBasedMovingPlatform : PlatformMovementScript
+{
+    [SerializeField] private float _directionY = 1;
+    private readonly string _layerToCheck = "Ground";
+
+    protected override void PrepareForMovement()
+    {
+    }
+
+    protected override void Move() {
+        Vector2 direction = new(0, _directionY);
+        _rb.velocity = direction * _movementSpeed;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider) {
+        if (LayerMask.NameToLayer(_layerToCheck) == collider.gameObject.layer) {
+            _directionY *= -1;
+        }
+    }
+}
