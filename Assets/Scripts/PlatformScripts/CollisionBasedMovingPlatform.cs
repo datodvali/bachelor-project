21e7862147b01ba6a4df95145c78ec0af408d1b7
daviddvali <dato11235813@gmail.com>
2024-08-22 23:27:31 +0400

    collision based platform should change directions when colliding with other platforms

--- Assets/Scripts/PlatformScripts/CollisionBasedMovingPlatform.cs
+++ Assets/Scripts/PlatformScripts/CollisionBasedMovingPlatform.cs
@@ -1,8 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionBasedMovingPlatform : PlatformMovementScript {
-    private readonly string _layerToCheck = "Ground";
+    private readonly List<string> _layersToCheck = new();
+
     [SerializeField] protected int _directionX = 1;
+
+    protected override void Awake() {
+        base.Awake();
+        _layersToCheck.Add("Ground");
+        _layersToCheck.Add("Platform");
+    }
 
     protected override void Move() {
         Vector2 direction = new(_directionX, 0);
@@ -10,8 +18,11 @@ public class CollisionBasedMovingPlatform : PlatformMovementScript {
     }
 
     public void OnTriggerEnter2D(Collider2D collider) {
-        if (LayerMask.NameToLayer(_layerToCheck) == collider.gameObject.layer) {
-            _directionX *= -1;
+        foreach (string layerName in _layersToCheck) {
+            if (LayerMask.NameToLayer(layerName) == collider.gameObject.layer) {
+                _directionX *= -1;
+                return;
+            }            
         }
     }
 }
